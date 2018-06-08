35a86d1954177e56c374dca807e86226ea25585b
Ambono <modpleh@yahoo.co.uk>
2018-06-08 08:06:00 +0100

    Update Palindrome.cs

--- Palindrome/Palindrome.cs
+++ Palindrome/Palindrome.cs
@@ -25,3 +25,17 @@ namespace palindrome
     }
   }
 }
+<!--
+  
+  in javascript
+  function palindrome(str) {
+    var len = str.length;
+    for ( var i = 0; i < Math.floor(len/2); i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+  
+  -->
